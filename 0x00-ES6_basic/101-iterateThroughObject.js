ec41641e31c3206d2ac021395e2df414f1594efe
elmouy <elmoutaouakkaly@gmail.com>
2024-03-04 15:42:30 -0800

    up

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-    
-    for (const category in reportWithIterator) {
-        if (Object.hasOwnProperty.call(reportWithIterator, category)) {
-            const employees = reportWithIterator[category];
-            result += employees.join(' | ') + ' | ';
-        }
-    }
-    
-    // Removing the trailing ' | ' from the result
-    result = result.slice(0, -3);
-    
-    return result;
+  return reportWithIterator.join(' | ');
 }
